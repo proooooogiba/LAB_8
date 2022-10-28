bdb5f4cb874ca0380918deba8571fc10ad73d51d
Ivan Pogiba <pogibuskaivan@gmail.com>
2022-10-28 17:05:46 +0300

    Functional test are added

--- test/controllers/calc_controller_test.rb
+++ test/controllers/calc_controller_test.rb
@@ -1,13 +1,30 @@
-require "test_helper"
+# frozen_string_literal: true
+
+require 'test_helper'
 
 class CalcControllerTest < ActionDispatch::IntegrationTest
-  test "should get input" do
+  test 'should get input' do
     get calc_input_url
     assert_response :success
   end
 
-  test "should get view" do
+  test 'should get view' do
     get calc_view_url
     assert_response :success
+  end
+
+  test 'should get 1 for view with 1' do
+    get '/calc/view', params: { num: 1 }
+    assert_equal(assigns[:result], [1])
+  end
+
+  test 'should get 1, 5, 6, 25, 76 for view with 100' do
+    get '/calc/view', params: { num: 100 }
+    assert_equal(assigns[:result], [1, 5, 6, 25, 76])
+  end
+
+  test 'should get 1, 5, 6, 25 for view with 50' do
+    get '/calc/view', params: { num: 50 }
+    assert_equal(assigns[:result], [1, 5, 6, 25])
   end
 end
