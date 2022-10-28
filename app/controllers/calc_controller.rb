cbb9db06d30a45b983bcfc2d1180f38305af4522
Ivan Pogiba <pogibuskaivan@gmail.com>
2022-10-28 11:56:41 +0300

    intermediate commit

--- app/controllers/calc_controller.rb
+++ app/controllers/calc_controller.rb
@@ -15,18 +15,13 @@ class CalcController < ApplicationController
           {type: @result.class.to_s, value: @result}
       end
     end
-    
   end
 
   private
 
   def automorf(n)
-    arr = Array.new
-    (1..n).each do |i|
-        if (i == i**2 % 10 || i == i**2 % 100 || i == i**2 % 1000)
-            arr.append(i)
-        end
-    end
-    return arr
+    arr = []
+    (1..n).each { |i| if (i**2).to_s.end_with?(i.to_s) then arr.append(i) end }
+    arr
   end
 end
