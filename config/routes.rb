174df412dadaf440d96d87c3207bce98adb545a1
Ivan Pogiba <pogibuskaivan@gmail.com>
2022-12-04 11:33:52 +0300

    Authrization is created

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
+  root to: 'pages#home'
+  devise_for :users
   get 'calc/input'
   get 'calc/view'
   get 'input/view'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  root to: "calc#input"
+  get 'calc/input'
   # Defines the root path route ("/")
   # root "articles#index"
 end
