4c7f6ef4266fd8a79a6c3baa7e88d7831566cad5
Ivan Pogiba <pogibuskaivan@gmail.com>
2022-12-04 15:35:33 +0300

    users list is added

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,10 @@ Rails.application.routes.draw do
   root to: 'calc#input'
   devise_for :users
   resources :users, only: [:index]
-  get 'calc/input'
+  get 'calc/input', as: :calc_input
   get 'calc/view'
   get 'input/view'
-  # get 'user/index', as: :user
+  get 'users/index', as: :user
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # Defines the root path route ("/")
   # root "articles#index"
